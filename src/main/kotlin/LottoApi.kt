c65d3c62dc7239f66e36e443c4172024829209b9
henick408 <marcel.snopkowski@gmail.com>
2026-01-06 04:18:17 +0100

    Obsługa Lotto i LottoPlus

--- src/main/kotlin/LottoApi.kt
+++ src/main/kotlin/LottoApi.kt
@@ -4,23 +4,6 @@ object LottoApi {
 
     private val service = ApiInstance.createService(LottoService::class.java)
 
-    private val rangeMap: Map<String, IntRange> = mapOf(
-        "miniLotto" to 1..42,
-        "lotto" to 1..49,
-        "euroJackpotFirst" to 1..50,
-        "euroJackpotSecond" to 1..12,
-        "ekstraPensjaFirst" to 1..35,
-        "ekstraPensjaSecond" to 1..4
-    )
-
-    private val numberAmountMap: Map<String, Int> = mapOf(
-        "miniLotto" to 5,
-        "lotto" to 6,
-        "euroJackpotFirst" to 5,
-        "euroJackpotSecond" to 2,
-        "ekstraPensjaFirst" to 5,
-        "ekstraPensjaSecond" to 1
-    )
 
     suspend fun getLastDraws(): List<DrawResponse>? {
         try {
@@ -63,16 +46,10 @@ object LottoApi {
     }
 
     suspend fun checkLastMiniLotto(numbers: List<Int>): WinInfo? {
-        if (numbers.size != numberAmountMap["miniLotto"]) {
-            return WinInfo(
-                info = "Niepoprawna ilość liczb"
-            )
-        }
 
-        if (!numbers.none { rangeMap["miniLotto"]?.contains(it) != true  }) {
-            return WinInfo(
-                info = "Niepoprawny zakres liczb"
-            )
+        val errorWinInfo = numbers.validateTicketData("MiniLotto")
+        if (errorWinInfo != null) {
+            return errorWinInfo
         }
 
         val draws = service.getLastDrawsInfoPerGame("MiniLotto")
@@ -86,12 +63,67 @@ object LottoApi {
         }
 
         return WinInfo(
-            winningNumbers = numbers,
-            numbersHitAmount = numbersHit,
-            isWon = true
+
+            winningNumbers = listOf(WinningNumbers(
+                numbers = numbers,
+                numbersHitAmount = numbersHit,
+                gameType = "MiniLotto",
+            ))
         )
     }
 
+    suspend fun checkLastLotto(numbers: List<Int>): WinInfo? {
+
+        val errorWinInfo = numbers.validateTicketData("Lotto")
+        if (errorWinInfo != null) {
+            return errorWinInfo
+        }
+
+        val draws = service.getLastDrawsInfoPerGame("Lotto")
+
+        val results = draws.first().results.first().resultsJson
+        val resultsPlus = draws[1].results.first().resultsJson
+
+        val numbersHit = numbers.filter { results.contains(it) }.size
+        val numbersHitPlus = numbers.filter { resultsPlus.contains(it) }.size
+
+        val winningNumbers: MutableList<WinningNumbers> = mutableListOf()
+
+        if (numbersHit >= 2) {
+            winningNumbers.add(WinningNumbers(numbers = numbers, numbersHitAmount = numbersHit, gameType = "Lotto"))
+        }
+        if (numbersHitPlus >= 2) {
+            winningNumbers.add(WinningNumbers(numbers = numbers, numbersHitAmount = numbersHitPlus, gameType = "LottoPlus"))
+        }
+        if (winningNumbers.isEmpty()) {
+            return null
+        }
+
+        return WinInfo(
+            winningNumbers = winningNumbers
+        )
+
+    }
+
+
+    private fun List<Int>.validateTicketData(gameType: String): WinInfo? {
+        if (this.size != numberAmountMap[gameType]) {
+            return WinInfo(
+                info = "Niepoprawna ilosc liczb"
+            )
+        }
+
+        if (!this.none { rangeMap[gameType]?.contains(it) != true }) {
+            return WinInfo(
+                info = "Niepoprawny zakres liczb"
+            )
+        }
+
+        return null
+    }
+
+    private fun List<DrawResponse>.getResults(): List<Int> =
+        this.first().results.first().resultsJson
 
 
 }
@@ -101,8 +133,34 @@ data class CheckResponse(
 )
 
 data class WinInfo(
-    val winningNumbers: List<Int> = listOf(),
+    val winningNumbers: List<WinningNumbers> = listOf(),
+    val info: String = ""
+)
+
+data class WinningNumbers(
+    val numbers: List<Int>,
     val numbersHitAmount: Int = 0,
-    val info: String = "",
-    val isWon: Boolean = false
+    val gameType: String = ""
+)
+
+private val games: Set<String> = setOf(
+    "Lotto", "LottoPlus", "MiniLotto", "EuroJackpot", "EkstraPensja"
+)
+
+private val rangeMap: Map<String, IntRange> = mapOf(
+    "MiniLotto" to 1..42,
+    "Lotto" to 1..49,
+    "EuroJackpotFirst" to 1..50,
+    "EuroJackpotSecond" to 1..12,
+    "EkstraPensjaFirst" to 1..35,
+    "EkstraPensjaSecond" to 1..4
+)
+
+private val numberAmountMap: Map<String, Int> = mapOf(
+    "MiniLotto" to 5,
+    "Lotto" to 6,
+    "EuroJackpotFirst" to 5,
+    "EuroJackpotSecond" to 2,
+    "EkstraPensjaFirst" to 5,
+    "EkstraPensjaSecond" to 1
 )
