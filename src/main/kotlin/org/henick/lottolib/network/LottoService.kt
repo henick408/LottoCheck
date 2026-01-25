6ec1322bfd84d0671380a09aaa797b27aecb4af5
henick408 <marcel.snopkowski@gmail.com>
2026-01-25 14:07:29 +0100

    Usunięto metodę getDrawsInfoByDatePerGame oraz DrawResponseByDatePerGame.kt Jest to niepotrzebne, bo teraz wyciąganie danych po grze oraz po dacie jest inaczej zaimplementowane (wyciagane danych po dacie a potem filtrowanie po typie gry). Zapomniałem w poprzednim commicie o tym, więc jest teraz

--- src/main/kotlin/org/henick/lottolib/network/LottoService.kt
+++ src/main/kotlin/org/henick/lottolib/network/LottoService.kt
@@ -1,7 +1,6 @@
 package org.henick.lottolib.network
 
 import org.henick.lottolib.network.dto.DrawResponse
-import org.henick.lottolib.network.dto.DrawResponseByDatePerGame
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -20,15 +19,5 @@ interface LottoService {
     suspend fun getDrawsInfoByDate(
         @Query("drawDate") drawDate: String
     ): Response<List<DrawResponse>>
-
-    @GET("draw-results/by-date-per-game?")
-    suspend fun getDrawsInfoByDatePerGame(
-        @Query("gameType") gameType: String,
-        @Query("drawDate") drawDate: String,
-        @Query("index") index: Int = 1,
-        @Query("size") size: Int = 1,
-        @Query("sort") sort: String = "drawSystemId",
-        @Query("order") order: String = "ASC"
-    ): Response<DrawResponseByDatePerGame>
 
 }
--- src/main/kotlin/org/henick/lottolib/network/dto/DrawResponseByDatePerGame.kt
+++
@@ -1,8 +0,0 @@
-package org.henick.lottolib.network.dto
-
-data class DrawResponseByDatePerGame(
-    val totalRows: Int,
-    val items: List<DrawResponse>,
-    val meta: Any,
-    val code: Int
-)
